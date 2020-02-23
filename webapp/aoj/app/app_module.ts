fa3ad0d89a0fd4bf0e453e886c3b096418d67a4b
Jiaqi Guo <jiaqi@cyclopsgroup.org>
2020-02-22 22:52:55 -0800

    Dependencies

--- webapp/aoj/app/app_module.ts
+++ webapp/aoj/app/app_module.ts
@@ -1,9 +1,14 @@
-import { NgModule } from '@angular/core';
-import { App } from './app';
+import {NgModule} from '@angular/core';
+import {App} from './app';
+import {BrowserModule} from '@angular/platform-browser';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [App],
-  imports: [],
+  imports: [
+    // BrowserModule,
+    // BrowserAnimationsModule
+  ],
   exports: [App],
   bootstrap: [App],
 })
