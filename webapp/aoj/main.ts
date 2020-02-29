b19a93a408fa88f192f64b9561f68150ac7b7696
Jiaqi Guo <jguo@chime.com>
2020-02-28 20:46:52 -0800

    Remove useless build targets

--- webapp/aoj/main.ts
+++ webapp/aoj/main.ts
@@ -1,6 +1,4 @@
 import { platformBrowser } from '@angular/platform-browser';
 import { AppModuleNgFactory } from './app/app_module.ngfactory';
-import { enableProdMode } from '@angular/core';
 
-enableProdMode()
 platformBrowser().bootstrapModuleFactory(AppModuleNgFactory);
