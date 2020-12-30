376b876bf51d3f9ac7202e775182860d67d78cd9
Jiaqi Guo <jguo@chime.com>
2020-12-29 16:45:25 -0800

    Add a demo proto

--- webapp/aoj/app/app.ts
+++ webapp/aoj/app/app.ts
@@ -1,5 +1,10 @@
 import { Component } from '@angular/core';
+import { Thing } from 'angular_on_java/webapp/aoj/app/thing_pb';
 
 @Component({ selector: 'app-component', templateUrl: 'app.html' })
 export class App {
+    constructor() {
+        const thing = new Thing();
+        console.log(`The thing: ${thing}`);
+    }
 }
